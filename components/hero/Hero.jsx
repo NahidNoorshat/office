a91433a56593325f6f40dadcce5d541d3282f3e5
NahidNoorshat <abunahid754@gmail.com>
2025-04-28 01:04:17 +0600

    Optimized video loading with Cloudinary CDN

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -7,14 +7,19 @@ const Hero = () => {
     <>
       <div className="relative min-h-screen w-screen  bg-black   flex items-center justify-center">
         <video
-          className="absolute top-0 left-0 w-full h-screen object-cover z-0 opacity-70 "
+          className="absolute top-0 left-0 w-full h-screen object-cover z-0 opacity-70"
           autoPlay
           loop
           muted
           playsInline
+          poster="/bacgroundvideo/optimized-thumbnail.jpg"
         >
-          <source src="/bacgroundvideo/0305.mp4" type="video/mp4" />
+          <source
+            src="https://res.cloudinary.com/dadozjlzb/video/upload/v1745779839/0305-compressed_zktg3x.mp4"
+            type="video/mp4"
+          />
         </video>
+
         <div className="relative   text-center">
           <h1 className=" text-8xl text-[#ffff]  leading-[89.95px] tracking-[3%] font-bold mx-2">
             Elevate Your Online Identity with
@@ -51,30 +56,6 @@ const Hero = () => {
             <Mainbutton />
           </div>
         </div>
-        {/* <div className=" max-w-7xl w-full h-full ">
-          <div className=" max-w-4xl w-full h-full flex flex-col items-center md:items-start justify-center gap-5 px-3 ">
-            <h1 className=" text-white  leading-6  ">
-              Welcome To
-              <span className=" text-primary-color text-base font-normal  ">
-                {" "}
-                Omega It Sys.
-              </span>
-            </h1>
-            <h1 className=" font-bold text-7xl text-white leading-[90px] ">
-              Designing the Future with Canvas Lancer
-            </h1>
-            <h1 className=" text-base font-normal leading-6 tracking-wider text-white ">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
-              ea neque mollitia consequatur ex odit amet quam accusantium et
-              officia.{" "}
-            </h1>
-            
-            <div className="flex gap-2">
-              <Mainbutton />
-              <Mainbutton />
-            </div>
-          </div>
-        </div> */}
       </div>
     </>
   );
